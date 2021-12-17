b7c7a62284df9f18046b4f4e730912419e440620
Sanjeev94 <sanjeev.dubey930@gmail.com>
2021-12-17 15:19:00 +0530

    Adding grid view center for clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAn2rvCztQlO47jm8S8DeNqocK9hlqBEpY",
+    authDomain: "whats-app-clone-b59aa.firebaseapp.com",
+    projectId: "whats-app-clone-b59aa",
+    storageBucket: "whats-app-clone-b59aa.appspot.com",
+    messagingSenderId: "905252278165",
+    appId: "1:905252278165:web:67d56cf3e16d3bac57b09c",
+    measurementId: "G-VQGT2R33ZP"
+  };
