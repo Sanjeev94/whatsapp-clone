172e032c2dee7dc0d70fdefd82a9f43b0e53ef4e
Sanjeev94 <sanjeev.dubey930@gmail.com>
2021-12-17 16:19:28 +0530

    Adding Sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
      // BEM naming Convention
     <div className="app">
-      <h1>Whats App Clone</h1>
+      {/* <h1>Whats App Clone</h1> */}
 
       <div className="app__body">
+        <Sidebar />
         {/* Sidebar */}
         {/* Chat */}
       </div>
